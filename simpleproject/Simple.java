c7cb3cd1f9bb26e961f236026466cfd280c332f8
prasadkorgaonkar <119840587+prasadkorgaonkar@users.noreply.github.com>
2022-12-06 13:10:53 +0530

    Commited by Prasad.

--- simpleproject/Simple.java
+++ simpleproject/Simple.java
@@ -1,7 +1,8 @@
 public class Simple {
 	public static void main(String args[])
 	 {
-		 System.out.print(Hello world);
+		System.out.print(Hello world);
+		System.out.print("Welcome to GitHub"):
 	 }
 
 }
